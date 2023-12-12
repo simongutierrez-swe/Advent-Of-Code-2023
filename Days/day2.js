94322755b7d118332e714a73251e470584b47649
SimonG <saguti15@g.holycross.edu>
2023-12-12 15:01:35 -0500

    SG: made files for input and days as well as started day 3

--- Days/day2.js
+++ Days/day2.js
@@ -29,9 +29,7 @@ Determine which games would have been possible if the bag had been loaded with o
 */
 
 const fs = require('fs');
-
 const input = fs.readFileSync('./input2.txt').toString().replace(/\n/g, '');
-
 
 const test = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green';
 
